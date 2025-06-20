dbe64c6326f8e46232a006ec5b3fbe4052ddaec5
rmi99 <rmigs999@gmail.com>
2025-06-21 05:14:30 +0530

    .

---
+++ Employee.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace WinFormsApp2
+{
+    partial class Employee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtContact = new TextBox();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            btnClear = new Button();
+            txtEmail = new TextBox();
+            txtName = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            txtNic = new TextBox();
+            dataGridEmployee = new DataGridView();
+            btnAdd = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            label4 = new Label();
+            txtId = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridEmployee).BeginInit();
+            SuspendLayout();
+            // 
+            // txtContact
+            // 
+            txtContact.Location = new Point(198, 250);
+            txtContact.Name = "txtContact";
+            txtContact.Size = new Size(264, 27);
+            txtContact.TabIndex = 24;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(54, 292);
+            label7.Name = "label7";
+            label7.Size = new Size(33, 20);
+            label7.TabIndex = 23;
+            label7.Text = "NIC";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(54, 292);
+            label6.Name = "label6";
+            label6.Size = new Size(0, 20);
+            label6.TabIndex = 22;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(54, 208);
+            label5.Name = "label5";
+            label5.Size = new Size(46, 20);
+            label5.TabIndex = 21;
+            label5.Text = "Email";
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(294, 372);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(94, 29);
+            btnClear.TabIndex = 18;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(195, 201);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(267, 27);
+            txtEmail.TabIndex = 17;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(198, 150);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(264, 27);
+            txtName.TabIndex = 16;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(54, 157);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 20);
+            label3.TabIndex = 15;
+            label3.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(54, 255);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 20);
+            label2.TabIndex = 14;
+            label2.Text = "Contact";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Poor Richard", 20F);
+            label1.Location = new Point(12, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(145, 38);
+            label1.TabIndex = 13;
+            label1.Text = "Employee";
+            // 
+            // txtNic
+            // 
+            txtNic.Location = new Point(198, 292);
+            txtNic.Name = "txtNic";
+            txtNic.Size = new Size(264, 27);
+            txtNic.TabIndex = 25;
+            // 
+            // dataGridEmployee
+            // 
+            dataGridEmployee.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridEmployee.Location = new Point(518, 50);
+            dataGridEmployee.Name = "dataGridEmployee";
+            dataGridEmployee.RowHeadersWidth = 51;
+            dataGridEmployee.Size = new Size(430, 379);
+            dataGridEmployee.TabIndex = 26;
+            dataGridEmployee.CellClick += dataGridEmployee_CellClick;
+            dataGridEmployee.CellContentClick += dataGridEmployee_CellContentClick;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(144, 372);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(94, 29);
+            btnAdd.TabIndex = 27;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(144, 419);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(94, 29);
+            btnEdit.TabIndex = 28;
+            btnEdit.Text = "Edit";
+            btnEdit.UseVisualStyleBackColor = true;
+            btnEdit.Click += btnEdit_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(294, 419);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(94, 29);
+            btnDelete.TabIndex = 29;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(54, 101);
+            label4.Name = "label4";
+            label4.Size = new Size(22, 20);
+            label4.TabIndex = 30;
+            label4.Text = "id";
+            label4.Click += label4_Click;
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(195, 94);
+            txtId.Name = "txtId";
+            txtId.ReadOnly = true;
+            txtId.Size = new Size(264, 27);
+            txtId.TabIndex = 31;
+            // 
+            // Employee
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(978, 463);
+            Controls.Add(txtId);
+            Controls.Add(label4);
+            Controls.Add(btnDelete);
+            Controls.Add(btnEdit);
+            Controls.Add(btnAdd);
+            Controls.Add(dataGridEmployee);
+            Controls.Add(txtNic);
+            Controls.Add(txtContact);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(btnClear);
+            Controls.Add(txtEmail);
+            Controls.Add(txtName);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Employee";
+            Text = "Employee";
+            ((System.ComponentModel.ISupportInitialize)dataGridEmployee).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtContact;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Button btnClear;
+        private TextBox txtEmail;
+        private TextBox txtName;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox txtNic;
+        private DataGridView dataGridEmployee;
+        private Button btnAdd;
+        private Button btnEdit;
+        private Button btnDelete;
+        private Label label4;
+        private TextBox txtId;
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WinFormsApp2
+{
+    public partial class Employee : Form
+    {
+
+        //string ConnectionString = "Data Source=DESKTOP-8K4V0F1;Initial Catalog=EmployeeManagement;Integrated Security=True";
+        string ConnectionString = "Server=localhost;Port=3306;Database=esoft;Uid=root;Pwd=;";
+        public Employee()
+        {
+            InitializeComponent();
+            LoadStudent();
+
+        }
+
+        private void dataGridEmployee_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            LoadStudent();
+        }
+        private void LoadStudent()
+        {
+            MessageBox.Show("Loading data from database...", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            using (MySqlConnection conn = new MySqlConnection(ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query1 = "SELECT * FROM student"; // MySQL
+
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(query1, conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    dataGridEmployee.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading data: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clearFields();
+
+        }
+
+        private void clearFields()
+        {
+            txtName.Clear();
+            txtEmail.Clear();
+            txtContact.Clear();
+            txtNic.Clear();
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            DataGridView dataGridView = new DataGridView();
+
+            using (
+                MySqlConnection conn = new MySqlConnection(ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    //mysql
+
+                    string query = "INSERT INTO student (name, email) VALUES (@name, @email)";//Mysql
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Student added successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    clearFields();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error connecting to database: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            };
+
+
+            //if (string.IsNullOrEmpty(txtName.Text) || string.IsNullOrEmpty(txtEmail.Text) || string.IsNullOrEmpty(txtContact.Text) || string.IsNullOrEmpty(txtNic.Text))
+            //{
+            //    MessageBox.Show("Please fill all fields", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            //    return;
+            //}
+        }
+
+        private void dataGridEmployee_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridEmployee.Rows[e.RowIndex];
+                txtId.Text = row.Cells["id"].Value.ToString();
+                txtName.Text = row.Cells["name"].Value.ToString();
+                txtEmail.Text = row.Cells["email"].Value.ToString();
+
+            }
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "UPDATE student SET name = @name, email = @email WHERE id = @id";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", txtId.Text);
+                    cmd.Parameters.AddWithValue("@name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    cmd.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show(rowsAffected + " Student(s) updated successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    }
+                    clearFields();
+                    LoadStudent();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error updating student: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            using MySqlConnection conn = new MySqlConnection(ConnectionString);
+            try {  conn.Open();
+
+                string query = "DELETE FROM student WHERE id = @id";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", txtId.Text);
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show(rowsAffected + " Student(s) deleted successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    clearFields();
+                    LoadStudent();
+                }
+                else
+                {
+                    MessageBox.Show("No student found with the given ID", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error connecting to database: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+    }
+}
